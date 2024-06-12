3ae20a264daf72433b51b8a253a4cd580f187604
Peter0x44 <peter0x44@disroot.org>
2024-06-12 16:15:50 +0100

    Build with SDL for Windows
    There are link errors to SDL's functions. Running make twice works, and
    the exectuable builds correctly. Don't ask me why it helps, I don't
    know.

--- tools/build-nzp-win32.sh
+++ tools/build-nzp-win32.sh
@@ -1,2 +1,5 @@
 cd ../engine
-make makelibs FTE_TARGET=win32 && make m-rel FTE_TARGET=win32 FTE_CONFIG=nzportable -j32
+make makelibs FTE_TARGET=win32_SDL2 && make m-rel FTE_TARGET=win32_SDL2 FTE_CONFIG=nzportable -j32
+# Running make once is not sufficient... there are link errors (undefined reference to SDL's functions).
+# Doing it twice works. I don't know why.
+make m-rel FTE_TARGET=win32_SDL2 FTE_CONFIG=nzportable -j32

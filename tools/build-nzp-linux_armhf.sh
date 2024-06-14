15f4495b63a35cb615f36de4efe6d3a3f16be819
Peter0x44 <peter0x44@disroot.org>
2024-06-14 03:09:40 +0100

    Give executables consistent names

--- tools/build-nzp-linux_armhf.sh
+++ tools/build-nzp-linux_armhf.sh
@@ -2,3 +2,4 @@ cd ../engine
 export CC=arm-linux-gnueabihf-gcc
 export STRIP=arm-linux-gnueabihf-strip
 make makelibs FTE_TARGET=SDL2 && make m-rel FTE_TARGET=SDL2 FTE_CONFIG=nzportable -j32
+mv release/nzportable-sdl2 release/nzportablearmhf-sdl

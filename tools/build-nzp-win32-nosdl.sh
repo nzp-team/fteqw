91e23786b4b3d8557bc421ebd8559264d407c7c2
MotoLegacy <motolegacy@proton.me>
2024-11-30 14:18:42 -0800

    Add pipeline for no-SDL2 win32/win64

---
+++ tools/build-nzp-win32-nosdl.sh
@@ -0,0 +1,2 @@
+cd ../engine
+make makelibs FTE_TARGET=win32 && make m-rel FTE_TARGET=win32 FTE_CONFIG=nzportable -j32
---
+++ tools/build-nzp-win64-nosdl.sh
@@ -0,0 +1,2 @@
+cd ../engine
+make makelibs FTE_TARGET=win64 && make m-rel FTE_TARGET=win64 FTE_CONFIG=nzportable -j32
